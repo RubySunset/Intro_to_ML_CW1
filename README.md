f5d56db7e74b5475390d1f14cd174c7938302a12
Daniel Coroama <daniel.coroama21@imperial.ac.uk>
2023-10-30 21:03:20 +0000

    Update Readme

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Intro_to_ML_CW1
+
+Overview of [test_all.py](test_all.py):
+- `train_and_test(dataset_path="wifi_db/clean_dataset.txt")` Tests the algorithm on a dataset and prints the evaluation metrics: 
+  - Total confusion matrix from summing all the matrices from each fold.
+  - Average accuracy.
+  - Average precision and recall per class (room).
+  - Average F1 score per class. This is by calculating the F1 score per class for each fold, then finding the average across folds.
+- Initialiser (`__name__==__main__`): Gives multiple options to the 
+
+### [**Evaluation Folder**](evaluation/)
+-----
+This folder contains the functions related to evaluation metric and k_fold testing.
+
+Functions in [eval.py](evaluation/eval.py):
+- `classify(attributes, tree)` Finds the corresponding label of a list of attributes to a decision tree recursively. Knows when it's reached a leaf node by checking type.
+- `evaluate(test_db, trained_tree)` Iterates through a dataset using `classify` for each row, while also updating the confusion matrix each iteration
+- `precision` and `recall` Both use a confusion matrix and also return the macro precision and recall
+- `f1_score_class(precision, recall)` Returns F1 score per class. Only this function for F1 score is used for efficiency so as to not calculate precision and recall metrics twice per class.
+
+Functions in [k_folds.py](evaluation/k_folds.py)
+- `indices_split_folds(fold_n, instances_n)` Shuffles indices and organises them into separate folds for each fold iteration.
+- `test_k_folds(dataset, k=10)` Tests the algorithm on a dataset for k-fold cross validation. Builds a tree for each fold, tests, and returns list of accuracies and confusion matrices for each fold.
--- test_all.py
+++ test_all.py
@@ -18,7 +18,7 @@ def train_and_test(dataset_path="wifi_db/clean_dataset.txt"):
     accuracies, confusions  = test_k_folds(dataset) 
 
     accuracy_avg = np.mean(accuracies)
-    print("Accuracy average: " + str(accuracy_avg))
+    print("\nAccuracy average: " + str(accuracy_avg))
     
     # Combined confusion matrix is the matrix obtained from the sum of all the confusion matrices
     combined_confusion = np.zeros((4, 4), dtype=np.int32)
@@ -28,45 +28,27 @@ def train_and_test(dataset_path="wifi_db/clean_dataset.txt"):
     print("\nSum Confusion: ")
     print(combined_confusion)
     print("")
-    ### These are calculations done by using the combined confusion matrix.
-    ### The preferred averages are by averaging the individual metrics from each tree iteration.
-    # confusion_accuracy = np.trace(combined_confusion)/np.sum(combined_confusion)
-    # print("\nAccuracy from confusion: " + str(confusion_accuracy)) # This is the same as average accuracy
-
-    # combined_precisions, precision_avg = eval.precision(combined_confusion)
-    # combined_recalls, recall_avg = eval.recall(combined_confusion)
-
-    # print("Micro-averaged precision: ")
-    # print(combined_precisions) # micro-averaged
-
-    # print("Micro_averaged recall: ")
-    # print(combined_recalls)
 
     # Precisions for each of the 4 classes
     precisions_matrix = np.zeros((4, len(confusions)))
     recalls_matrix = np.zeros((4, len(confusions)))
-    # f1_matrix = np.zeros((4, len(confusions)))
     f1_matrix = np.zeros((4, len(confusions)))
 
     for i, matrix in enumerate(confusions):
         new_precisions = eval.precision(matrix)[0]
         new_recalls = eval.recall(matrix)[0]
-        # new_f1 = eval.f1_score_confusion(matrix)[0] ### MAKE this more efficient by not calculating precision and recall twice.
         for class_n in range(len(confusions[0])):
             precisions_matrix[class_n, i] = new_precisions[class_n] # Precision of class class_n+1 becomes a part of precisions matrix.
             recalls_matrix[class_n, i] = new_recalls[class_n]
-            # f1_matrix[class_n, i] = new_f1[class_n]
             f1_matrix[class_n, i] = eval.f1_score_class(new_precisions[class_n], new_recalls[class_n])
 
     
     precision_matrix_avg = []
     recall_matrix_avg = []
-    # f1_matrix_avg = []
     f1_matrix_avg = []
     for class_n_p, class_n_r, class_n_ef in zip(precisions_matrix, recalls_matrix, f1_matrix):
         precision_matrix_avg.append(np.mean(class_n_p))
         recall_matrix_avg.append(np.mean(class_n_r))
-        # f1_matrix_avg.append(np.mean(class_n_f))
         f1_matrix_avg.append(np.mean(class_n_ef))
         
 
@@ -75,7 +57,6 @@ def train_and_test(dataset_path="wifi_db/clean_dataset.txt"):
     print("Averaged recall per class: ")
     print(recall_matrix_avg)
     print("Averaged F1 score per class: ")
-    # print(f1_matrix_avg)
     print(f1_matrix_avg)
 
     ### These are calculations done by using the combined confusion matrix.
